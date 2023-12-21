721037c679c9aa9a0538f3e5e11033caae3a6499
clement <clementroland52@gmail.com>
2023-12-21 15:39:40 -0400

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+let heroeCount =2;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tour-of-heroes';
+  /*title = 'la tour des 1 heros';*/
+  titlestyle = {'font-family' : 'Arial', 'font-size' : '2rem'};
+  username = 'anonymous';
+  heroe =['Batman','Superman','Wonder-Woman','IronMan'];
+  heroToAdd = '';
+  mechants = ['kang','joker'];
+  mechantToAdd = '';
+  AddHero(){
+    this.heroe.push(this.heroToAdd);
+
+//this.title = 'La tour des ' + this.heroe.length + ' héros';
+
+  //this.title = 'La tour des' + (heroeCount++) + 'heros';
 }
+AddMechant(){
+  this.mechants.push(this.mechantToAdd);
+}
+}
+
